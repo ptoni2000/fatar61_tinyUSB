5cd5a9d98aba2812ebfab4a61d92b9e5c0293450
Pierantonio Tabaro <toni.tabaro@gmail.com>
2021-10-28 10:01:20 +0200

    velocity con tabella precalcolata

--- Core/Src/keyboard.c
+++ Core/Src/keyboard.c
@@ -23,9 +23,9 @@ typedef enum {
 } event_t;
 
 typedef struct {
-	char midi_note :8;
-	state_t state :4; // Bit fields
-	unsigned int t :7; // Lines up nicely to 16bits, t overflows at 4096
+	char 	 midi_note;
+	state_t  state :4; 	// Bit fields
+	uint32_t t; 		// Lines up nicely to 16bits, t overflows at 4096
 } midikey_t;
 
 typedef struct {
@@ -44,6 +44,20 @@ bank_t prev_banks[NUM_BANKS];
 uint8_t program = 0;
 uint8_t const cable_num = 0; // MIDI jack associated with USB endpoint
 uint8_t const channel = 1; // 0 for channel 1
+//Velocity curves
+//To save on calculation effort for the CPU, these are precalculated in Excel, the index is the number of milliseconds
+//between contact, 0-80ms.
+#define MAX_VEL_CURVE_INDEX 80
+const uint8_t linearCurve[MAX_VEL_CURVE_INDEX+1]={127, 127, 127, 125, 124, 122, 121, 119, 118, 116, 114, 113, 111, 110, 108, 107, 105, 103, 102, 100, 99, 97, 95, 94, 92, 91, 89, 88, 86, 84, 83, 81, 80, 78, 77, 75, 73, 72, 70, 69, 67, 66, 64, 62, 61, 59, 58, 56, 54, 53, 51, 50, 48, 47, 45, 43, 42, 40, 39, 37, 36, 34, 32, 31, 29, 28, 26, 25, 23, 21, 20, 18, 17, 15, 13, 12, 10, 9, 7, 6, 4};
+const uint8_t convexCurve[MAX_VEL_CURVE_INDEX+1]={127, 127, 127, 127, 127, 127, 127, 126, 126, 126, 125, 125, 125, 124, 123, 123, 122, 121, 121, 120, 119, 118, 117, 116, 115, 114, 113, 112, 111, 109, 108, 107, 105, 104, 102, 101, 99, 98, 96, 94, 93, 91, 89, 87, 86, 84, 82, 80, 78, 76, 74, 72, 70, 68, 66, 63, 61, 59, 57, 54, 52, 50, 48, 45, 43, 41, 38, 36, 33, 31, 29, 26, 24, 21, 19, 16, 14, 11, 9, 6, 4};
+const uint8_t saturatedCurve[MAX_VEL_CURVE_INDEX+1]={127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 126, 125, 125, 124, 122, 121, 119, 118, 116, 114, 111, 109, 106, 104, 101, 98, 94, 91, 87, 84, 80, 76, 72, 68, 64, 60, 55, 51, 47, 42, 37, 33, 28, 23, 18, 14, 9, 4};
+const uint8_t concaveCurve[MAX_VEL_CURVE_INDEX+1]={127, 127, 127, 122, 116, 111, 106, 102, 97, 93, 88, 84, 80, 76, 73, 69, 66, 62, 59, 56, 53, 50, 48, 45, 43, 40, 38, 36, 34, 32, 30, 28, 26, 25, 23, 22, 21, 19, 18, 17, 16, 15, 14, 13, 12, 11, 11, 10, 9, 9, 8, 8, 7, 7, 7, 6, 6, 6, 5, 5, 5, 5, 5, 5, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4};
+const uint8_t* velocityCurves[]={
+  linearCurve,
+  convexCurve,
+  saturatedCurve,
+  concaveCurve
+};
 
 void initialize() {
 	tu_printf("%s\n", __func__);
@@ -65,33 +79,38 @@ void initialize() {
 }
 
 void trigger(midikey_t *key, event_t event) {
+	uint32_t elapsed;
+	uint8_t  velocity;
+	const uint8_t* curve=velocityCurves[1];
 
 	if (event == KEY_PRESSED) {
 		key->state = KEY_IS_GOING_DOWN;
 		tu_printf("GOING DOWN %d\n", key->midi_note);
-
+		key->t = HAL_GetTick();
 	} else if (event == KEY_DOWN) {
 		if (key->state == KEY_IS_GOING_DOWN) {
 			key->state = KEY_IS_DOWN;
+			elapsed = HAL_GetTick() - key->t;
+			velocity=curve[tu_min32(MAX_VEL_CURVE_INDEX, elapsed)];
 			// Send Note On on channel 1.
-			uint8_t note_on[3] =
-					{ 0x90 | channel, key->midi_note, 127 - key->t };
+			uint8_t note_on[3] = { 0x90 | channel, key->midi_note, velocity };
 			tud_midi_stream_write(cable_num, note_on, 3);
-			tu_printf("DN %d %d\n", key->midi_note, 127 - key->t);
+			tu_printf("DN %d %d %ld\n", key->midi_note, velocity, elapsed);
 			key->t = 0;
 		}
 	} else if (event == KEY_RELEASED) {
 		key->state = KEY_IS_GOING_UP;
 		tu_printf("GOING UP %d\n", key->midi_note);
-
+		key->t = HAL_GetTick();
 	} else if (event == KEY_UP) {
 		if (key->state == KEY_IS_GOING_UP) {
 			key->state = KEY_IS_UP;
+			elapsed = HAL_GetTick() - key->t;
+			velocity=curve[tu_min32(MAX_VEL_CURVE_INDEX, elapsed)];
 			// Send Note Off on channel 1.
-			uint8_t note_off[3] =
-					{ 0x80 | channel, key->midi_note, 127 - key->t };
+			uint8_t note_off[3] = { 0x80 | channel, key->midi_note, velocity };
 			tud_midi_stream_write(cable_num, note_off, 3);
-			tu_printf("UP %d %d\n", key->midi_note, 127 - key->t);
+			tu_printf("UP %d %d %ld\n", key->midi_note, velocity, elapsed);
 			key->t = 0;
 		}
 	}
